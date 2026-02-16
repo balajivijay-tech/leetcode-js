d7d47856a737005c0885d031766ae1fc34270d11
balajivijay-tech <53026620+balajivijay-tech@users.noreply.github.com>
2026-02-16 08:40:59 +0530

    Time: 1 ms (49.43%) | Memory: 58.4 MB (36.48%) - LeetSync

---
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function(nums) {
+    let i = 0, j = 1
+    while(j<nums.length){
+        if(nums[i] !== nums[j]){
+            i++
+            nums[i] = nums[j]
+        }
+        j++
+    }
+    return i+1
+};
